fb7b6011f4f8301a96a3e61a8eecf1e76b5b2dd7
x3xgit <xyndex.shuvar@gmail.com>
2024-07-28 23:46:40 +0300

    1.1 - ALPHA

--- src/main/java/techcommunity/net/techbungee/TechBungee.java
+++ src/main/java/techcommunity/net/techbungee/TechBungee.java
@@ -1,26 +1,10 @@
 package techcommunity.net.techbungee;
 
-import com.google.common.io.ByteArrayDataOutput;
-import com.google.common.io.ByteStreams;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import techcommunity.net.techbungee.api.TechAPI;
 
 public final class TechBungee {
-    private JavaPlugin plugin;
-
-    public TechBungee(JavaPlugin plugin) {
-        this.plugin = plugin;
-
-        plugin.getServer().getMessenger().registerOutgoingPluginChannel(plugin, "BungeeCord");
-    }
-
-    public void transferTo(Player player, String serverName) {
-        if (player.isOnline()) {
-            ByteArrayDataOutput out = ByteStreams.newDataOutput();
-            out.writeUTF("ConnectOther");
-            out.writeUTF(player.getName());
-            out.writeUTF(serverName);
-            player.sendPluginMessage(plugin, "BungeeCord", out.toByteArray());
-        }
+    public TechAPI getAPI(JavaPlugin plugin) {
+        return new TechAPI(plugin);
     }
 }
