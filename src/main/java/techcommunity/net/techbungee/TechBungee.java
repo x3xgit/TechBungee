5c1a4ec04bea5ecb321a52753f1a65fa2cf3e176
x3xgit <xyndex.shuvar@gmail.com>
2024-07-28 23:25:00 +0300

    1.0.5

--- src/main/java/techcommunity/net/techbungee/TechBungee.java
+++ src/main/java/techcommunity/net/techbungee/TechBungee.java
@@ -1,30 +1,10 @@
 package techcommunity.net.techbungee;
 
-//import com.google.common.io.ByteArrayDataOutput;
-//import com.google.common.io.ByteStreams;
-//import org.bukkit.entity.Player;
-//import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.JavaPlugin;
+import techcommunity.net.techbungee.api.API;
 
 public final class TechBungee {
-    public static int getInt() {
-        return 1488;
+    public static API getAPI(JavaPlugin plugin) {
+        return new API(plugin);
     }
-
-    /*private JavaPlugin plugin;
-
-    public TechBungee(JavaPlugin plugin) {
-        this.plugin = plugin;
-
-        plugin.getServer().getMessenger().registerOutgoingPluginChannel(plugin, "BungeeCord");
-    }
-
-    public void transferTo(Player player, String serverName) {
-        if (player.isOnline()) {
-            ByteArrayDataOutput out = ByteStreams.newDataOutput();
-            out.writeUTF("ConnectOther");
-            out.writeUTF(player.getName());
-            out.writeUTF(serverName);
-            player.sendPluginMessage(plugin, "BungeeCord", out.toByteArray());
-        }
-    }*/
 }
---
+++ src/main/java/techcommunity/net/techbungee/api/API.java
@@ -0,0 +1,26 @@
+package techcommunity.net.techbungee.api;
+
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class API {
+    private JavaPlugin plugin;
+
+    public API(JavaPlugin plugin) {
+        this.plugin = plugin;
+
+        plugin.getServer().getMessenger().registerOutgoingPluginChannel(plugin, "BungeeCord");
+    }
+
+    public void transferTo(Player player, String serverName) {
+        if (player.isOnline()) {
+            ByteArrayDataOutput out = ByteStreams.newDataOutput();
+            out.writeUTF("ConnectOther");
+            out.writeUTF(player.getName());
+            out.writeUTF(serverName);
+            player.sendPluginMessage(plugin, "BungeeCord", out.toByteArray());
+        }
+    }
+}
